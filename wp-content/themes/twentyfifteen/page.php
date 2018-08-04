1bce1fa27e0c9b1d342558cea8a72bd2f60e5922
Matthew Eldredge <mattheweldredge@gmail.com>
2018-08-04 15:29:49 -0700

    demonstrate customizing the page.php template

--- wp-content/themes/twentyfifteen/page.php
+++ wp-content/themes/twentyfifteen/page.php
@@ -1,38 +1,12 @@
-<?php
-/**
- * The template for displaying pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages and that
- * other "pages" on your WordPress site will use a different template.
- *
- * @package WordPress
- * @subpackage Twenty_Fifteen
- * @since Twenty Fifteen 1.0
- */
+<?php 
+    get_header();
 
-get_header(); ?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-		<?php
-		// Start the loop.
-		while ( have_posts() ) : the_post();
-
-			// Include the page content template.
-			get_template_part( 'content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		// End the loop.
-		endwhile;
-		?>
-
-		</main><!-- .site-main -->
-	</div><!-- .content-area -->
-
-<?php get_footer(); ?>
+    while (have_posts(  )) {
+        the_post(  ); ?>
+        <h1>This is a page</h1>
+        <h2>This is a page</h2>
+        <?php the_content(); ?>
+    <?php }
+    
+    get_footer();
+?>
