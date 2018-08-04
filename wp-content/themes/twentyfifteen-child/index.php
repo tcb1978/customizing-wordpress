4b2b6bc1e038a3bb4677ebe61b1a848852f3fd14
Matthew Eldredge <mattheweldredge@gmail.com>
2018-08-04 15:48:07 -0700

    include the index.php template with an echo of "<h1>TEST</h1>"

--- wp-content/themes/twentyfifteen-child/index.php
+++ wp-content/themes/twentyfifteen-child/index.php
@@ -30,7 +30,7 @@ get_header(); ?>
 			<?php
 			// Start the loop.
 			while ( have_posts() ) : the_post();
-
+				echo '<h1>TEST</h1>';
 				/*
 				 * Include the Post-Format-specific template for the content.
 				 * If you want to override this in a child theme, then include a file
